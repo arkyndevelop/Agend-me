fc6c7d780d3a229b311810258c98a694d68b1639
Gabriel <gabriel_martinhoq@hotmail.com>
2025-08-28 19:57:04 -0300

    first commit

---
+++ src/main/java/br/com/agend/me/AgendMeApplication.java
@@ -0,0 +1,13 @@
+package br.com.agend.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agend/me/AgendMeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agend.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
